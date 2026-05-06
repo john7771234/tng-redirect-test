bd1d0e3ae8483ba7694e98d2bf349f6f0b68c56b
john7771234 <l5544john@gmail.com>
2026-05-06 21:31:11 +0800

    Refactor handler function to simplify response logic

--- api/packet.js
+++ api/packet.js
@@ -1,54 +1,18 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { token } = req.query;
 
-  if (!token) {
-    return res.status(400).send('Missing token');
-  }
+  const link = `https://links.tngdigital.com.my/moneypacket/${token}`;
 
-  try {
-    const response = await fetch(
-      `https://links.tngdigital.com.my/moneypacket/${token}`,
-      { redirect: 'follow' }
-    );
-
-    const finalUrl = new URL(response.url);
-    const hash = finalUrl.hash;
-    const params = new URLSearchParams(hash.split('?')[1]);
-
-    const p = params.get('p');
-    const v = params.get('v') || '2';
-
-    if (!p) {
-      return res.send('Invalid packet');
-    }
-
-    const deepLink = `tngdwallet://client/dl/transfer/moneyPacket/claim?p=${p}&v=${v}`;
-    const fallback = `https://links.tngdigital.com.my/moneypacket/${token}`;
-
-    res.setHeader('Content-Type', 'text/html');
-    res.send(`
-      <!DOCTYPE html>
-      <html>
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`
+    <html>
       <body>
         <h3>Opening TNG...</h3>
-        <button onclick="openApp()">Open TNG</button>
-
         <script>
-          function openApp() {
-            window.location.href = "${deepLink}";
-            setTimeout(() => {
-              window.location.href = "${fallback}";
-            }, 1500);
-          }
-
-          // auto try
-          openApp();
+          window.location.href = "${link}";
         </script>
+        <a href="${link}">Click here</a>
       </body>
-      </html>
-    `);
-
-  } catch (e) {
-    res.status(500).send('Error');
-  }
+    </html>
+  `);
 }
